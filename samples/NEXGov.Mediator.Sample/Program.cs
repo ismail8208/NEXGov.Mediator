c294f29015c1e3f8e00361e670673f29b7a80fa8
i.alahmad <i.alahmad@manteq-me.com>
2026-08-19 15:59:38 +0300

    MED-001 Project Foundation

--- samples/NEXGov.Mediator.Sample/Program.cs
+++ samples/NEXGov.Mediator.Sample/Program.cs
@@ -1,3 +1,4 @@
 // NEXGov.Mediator sample application.
 // Mediator usage examples will be added as the public API is implemented.
 Console.WriteLine("NEXGov.Mediator sample — mediator usage examples coming soon.");
+
